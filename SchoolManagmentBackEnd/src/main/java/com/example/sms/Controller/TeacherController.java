7c4fb062ecb4e6b218b17e1651eb24e5a18db0a9
Eibad <45168328+Eibad@users.noreply.github.com>
2020-09-16 20:59:03 +0500

    push backend

---
+++ SchoolManagmentBackEnd/src/main/java/com/example/sms/Controller/TeacherController.java
@@ -0,0 +1,54 @@
+package com.example.sms.Controller;
+
+
+import com.example.sms.DTO.StudentDTO;
+import com.example.sms.DTO.TeacherDTO;
+import com.example.sms.MODEL.Student;
+import com.example.sms.MODEL.Teacher;
+import com.example.sms.REPOSITORY.StudentRepository;
+import com.example.sms.REPOSITORY.TeacherRepository;
+import com.example.sms.SERVICE.StudentService;
+import com.example.sms.SERVICE.TeacherService;
+import com.sun.istack.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api")
+public class TeacherController {
+
+    @Autowired
+    TeacherRepository teacherRepository;
+
+    @Autowired
+    TeacherService teacherService;
+
+    @PostMapping("/teacher")
+    private ResponseEntity<Teacher> save(@RequestBody TeacherDTO teacherDTO){
+        return teacherService.saveTeacher(teacherDTO);
+    }
+
+    @PutMapping("/teacher/{id}")
+    private ResponseEntity<Teacher> update(@NotNull @RequestBody TeacherDTO teacherDTO, @NotNull @PathVariable("id") long id){
+        return teacherService.updateTeacher(id,teacherDTO);
+    }
+
+    @GetMapping("/teacher/{id}")
+    private ResponseEntity<Teacher> getById(@NotNull @PathVariable("id") long id){
+        return teacherService.getById(id);
+    }
+
+    @GetMapping("/teacher")
+    private ResponseEntity<List <Teacher>> getAll(){
+        return teacherService.getAll();
+    }
+
+    @GetMapping("/teacher/{name}")
+    private ResponseEntity<List<Teacher>> searchByName(@NotNull @PathVariable("name") String name){
+        return  teacherService.searchByName(name);
+    }
+}
